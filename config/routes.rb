664b44f9fb7c3d865a0574558bd15502f8f80e6d
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-05 11:18:35 +0300

    creating named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'fixed_pages/home'
+ root 'fixed_pages#home'
 
-  get 'fixed_pages/faq'
+  get 'faq' => 'fixed_pages#faq'
 
-  get 'fixed_pages/about'
+  get 'about' => 'fixed_pages#about'
 
-  get 'fixed_pages/contact'
+  get 'contact' => 'fixed_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
