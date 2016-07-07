fbd38e11674c9a839859ab2451d9463816bfb9a0
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-07 23:17:10 +0300

    test for valid signup information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "learn it live it"
       redirect_to @user
     else
       render 'new'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,4 +11,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information " do
+    get signup_url
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "stella",
+                                            email: "ste@gmail.com",
+                                            password: "rainbow",
+                                            password_confirmation: "rainbow"}
+    end
+    assert_template 'users/show'
+  end
  end
