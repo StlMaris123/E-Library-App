03a5514cce82330c9c26ca4064feb9dabb87dda3
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-07 08:25:16 +0300

    test for name and email presence

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "stella", email: "stellanajage@gmail.com")
+ end
+  test "user should be valid" do
+    assert @user.valid?
+  end
+test "User's name should be present" do
+  @user.name = ""
+  assert_not @user.valid?
+end
+test "User's email should be present" do
+  @user.email = ""
+  assert_not @user.valid?
+end
+
 end
