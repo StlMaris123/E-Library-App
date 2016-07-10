836f8586944649db70cb3f2547d995d1fb5a582d
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-10 22:35:51 +0300

    test for logging in with valid information

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:stella)
+  end
+
   test "login with invalid information " do
     get login_path
     assert_template 'sessions/new'
@@ -10,4 +14,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
- end
+  test "login with valid information" do
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+end
