537bd6aa13f47727c22cd37c2d102ee54ea33ae6
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-12 12:36:02 +0300

    test for persistent sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      raise
+      
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
