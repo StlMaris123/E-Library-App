9fd7a06e3c8909ca0c609fc7d9b0d6dbfb1d1f4d
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-12 14:24:28 +0300

    styling and adding an image

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,6 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
