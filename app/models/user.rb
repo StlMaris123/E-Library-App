da9f0236cdef231f9d156fbd29b303c6cc9929b8
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-12 07:57:37 +0300

    adding a method for generating tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,7 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
