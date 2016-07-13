9c0d552d3e9dac83f7030e8c42f0ec722aeea1d3
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-13 13:28:25 +0300

    adding the update action in users contoller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,22 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "learn it live it"
+      flash[:success] = "Karibu, learn it live it"
       redirect_to @user
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #handle a successful update
+    else
+      render 'edit'
     end
   end
 private
