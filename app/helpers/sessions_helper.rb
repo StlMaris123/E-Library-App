03ddee0d7de7843e3568113da7ae7e70ea143835
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-13 18:13:44 +0300

    index action to display all users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,57 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+
+  #returns true if the given user is the current user
+  module SessionsHelper
+    #Logs in the given user
+    def log_in(user)
+      session[:user_id] = user.id
+    end
+    #Remembers a user in a persistent session
+    def remember(user)
+      user.remember
+      cookies.permanent.signed[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+    end
+
+    #returns true if the given user is the current user
+    def current_user?(user)
+      user == current_user
+    end
+    #Returns the user corresponding to the remember token cookie
+    def current_user
+      if (user_id = session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+      elsif (user_id = cookies.signed[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
+        end
+      end
+    end
+    def logged_in?
+      !current_user.nil?
+    end
+
+    #forgets a persistent session
+    def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+    end
+
+    #logs out the current user
+    def log_out
+      forget(current_user)
+      session.delete(:user_id)
+      @current_user = nil
+    end
+  end
+  def current_user?(user)
+    user == current_user
+  end
   #Returns the user corresponding to the remember token cookie
   def current_user
     if (user_id = session[:user_id])
@@ -37,5 +88,13 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  #Redirects to stored location(or the default location)
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwwarding_url)
+  end
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,42 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:stella)
+    @other_user = users(:boris)
+  end
+
+  test "should redirect index when not logged in" do
+    get users_url
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get new_user_url 
     assert_response :success
   end
+  
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user), id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  test "should redirect update when not logged in" do
+    patch user_path(@user), id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user), id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+end
 
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
