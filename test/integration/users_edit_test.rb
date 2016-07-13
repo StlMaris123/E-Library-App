18b83c7bbb94c9d37b63ab0ce884f4245fbd94be
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-13 14:33:33 +0300

    allowing users to login before editing their profiles

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -5,6 +5,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user =users(:stella)
   end
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: "",
@@ -12,6 +13,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     password: "rain",
                                     password_confirmation: "bow"}
     assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Stella Njage"
+    email = "stellanjage@yahoo.com"
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password: "",
+                                    password_confirmation: ""}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
   end
 end
 
