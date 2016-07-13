26860525139a7350d9493f15d815a2d4fa613d95
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-13 21:42:27 +0300

    test for admin access control

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,4 +39,17 @@ end
     assert flash.empty?
     assert_redirected_to root_url
   end
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user), id: @user
+    end
+    assert_redirected_to login_url
+ end
+  test "should redirect destroy when logged in as a non admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user), id: @user
+    end
+    assert_redirected_to root_url
+  end
 end
