675cdd19aa0220d3777ca41f9a81820f867b6c3b
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-13 16:44:41 +0300

    testing that edit and update are protected

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:stella)
+  end
   test "should get new" do
     get login_url
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user), id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  test "should redirect update when not logged in" do
+    patch user_path(@user), id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
 end
