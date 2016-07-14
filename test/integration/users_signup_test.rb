4ce0172834e88555bbfae87f7eb51563d26c2436
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-14 18:16:41 +0300

    adding account activation to users signup test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
   test "invalid signup information " do
     get new_user_url
     assert_no_difference 'User.count' do
@@ -12,15 +15,32 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test "valid signup information " do
+  test "valid signup information with account activation " do
     get new_user_url
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: "stella",
-                                            email: "ste@gmail.com",
-                                            password: "rainbow",
-                                            password_confirmation: "rainbow"}
+      post users_path, user: { name: "stella",
+                               email: "ste@gmail.com",
+                               password: "rainbow",
+                               password_confirmation: "rainbow"}
     end
-    # assert_template 'users/show'
-    # assert is_logged_in?
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation.
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path("invalid token")
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
+
