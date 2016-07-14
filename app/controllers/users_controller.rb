cb1e3b5587dddc4eb9f72c63f98c46bbd60d0634
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-14 07:59:10 +0300

    adding extra users to the fixture

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def new
