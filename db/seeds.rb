dc3a114826338eff2b480bf7ca3e5cd015afa761
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-14 07:38:46 +0300

    adding sample users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-t# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Stella Njage",
+             email: "stellanjage@yahoo.com",
+             password: "raibow",
+             password_confirmation: "rainbow")
+29.times do |n|
+  name = Faker::Name.name
+  email = "stellanjage-#{n+1}@yahoo.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password:             password,
+              password_confirmation: password)
+end
