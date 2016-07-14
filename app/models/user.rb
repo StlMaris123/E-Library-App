9744c13ab22173bf27408068bc8459c1d3d4e7e9
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-14 10:14:16 +0300

    adding account activation code to User model\

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length:{ maximum: 50 }
-  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 250},
     format: { with: VALID_EMAIL_REGEX },
@@ -33,4 +34,16 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  private
+  #converts email to all lower_case
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  #creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
+
