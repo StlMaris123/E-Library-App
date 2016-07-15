441029078ebedabc77313ff500b8d24f68e45239
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-15 16:05:32 +0300

    migration for model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714160245) do
+ActiveRecord::Schema.define(version: 20160715125127) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer  "ISBN"
+    t.string   "title"
+    t.string   "description"
+    t.integer  "quantity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
