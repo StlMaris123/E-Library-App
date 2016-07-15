c59cc18ef3dbd9a7ae67226e80f6d02e477b6b9f
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-15 06:54:32 +0300

    adding password reset for plain-text email template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,9 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "Account activation"
   end
 
-  def password_reset
-    @greeting = "Hi"
+  def password_reset(user)
+    @user = user
 
-    mail to: "to@example.org"
+    mail to: user.email, subject: "Password reset"
   end
 end
