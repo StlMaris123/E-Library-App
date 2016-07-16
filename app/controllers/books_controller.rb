efe1220bc6b955458d6d79b4f4b4a68e87abd68f
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-16 07:24:13 +0300

    form for updating a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,28 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book was successfully added"
+      redirect_to @user 
+      else
+        render 'new'
+      end
+  end
+
 
   def edit
+    @book = Book.find_by(params[:title])
   end
 
   def show
 
   end
 end
+  private
+  def book_params
+    params.require(:book).permit(:ISBN, :title, 
+                                 :description, :quantity)
+  end
+
