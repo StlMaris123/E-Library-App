f67949fab48ab2afc6205d6231ed4d2a00744be3
StlMaris123 <stellamarisnjage@gmail.com>
2016-07-16 07:43:05 +0300

    displaying a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,15 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
   def create
     @book = Book.new(book_params)
     if @book.save
       flash[:success] = "Book was successfully added"
       redirect_to @user 
-      else
-        render 'new'
-      end
+    else
+      render 'new'
+    end
   end
 
 
@@ -21,23 +22,26 @@ class BooksController < ApplicationController
     @book = Book.find_by(params[:id])
   end
 
+  def destroy
+  end
+
   def update
     @book = Book.find_by(params[:id])
     if @book.update_attributes(book_params)
       flash[:success] = "Book was successfully updated"
-     redirect_to @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
 
   def show
-
+    @book = Book.find_by(params[:id])
   end
 end
-  private
-  def book_params
-    params.require(:book).permit(:ISBN, :title, 
-                                 :description, :quantity)
-  end
+private
+def book_params
+  params.require(:book).permit(:ISBN, :title, 
+                               :description, :quantity)
+end
 
