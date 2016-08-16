b1ff021819be5d9f3ec1fca1e390a5a6c0d3228f
StlMaris123 <stellamarisnjage@gmail.com>
2016-08-16 11:32:36 +0300

    define gravatar-for method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,10 @@ module UsersHelper
   def admin_user?
     current_user.admin?
   end
+
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
