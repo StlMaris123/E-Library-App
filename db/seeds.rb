8ea26085fe6b422d4b22ff4d8607efc89a88505f
StlMaris123 <stellamarisnjage@gmail.com>
2016-09-26 17:21:58 +0300

    remove fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,23 @@ User.create!(name: "Stella Njage",
              email: "stellanjage@yahoo.com",
              password: "rainbow",
              password_confirmation: "rainbow",
-             mobile_number: "0123456789",
+             mobile_number: "0710864761",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
-29.times do |n|
-  name = Faker::Name.name
-  email = "stellanjage-#{n+1}@yahoo.com"
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password:             password,
-               password_confirmation: password,
-               mobile_number: "1234567890",
-               activated: true,
-               activated_at: Time.zone.now)
-end
+# 29.times do |n|
+#   name = Faker::Name.name
+#   email = "stellanjage-#{n+1}@yahoo.com"
+#   password = "password"
+#   User.create!(name: name,
+#                email: email,
+#                password:             password,
+#                password_confirmation: password,
+#                mobile_number: "1234567890",
+#                activated: true,
+#                activated_at: Time.zone.now)
+# end
 100.times do |b|
   isbn = Faker::Code.isbn
   title = Faker::Book.title
