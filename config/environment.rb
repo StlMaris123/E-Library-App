a2cc43709ce87f7a59cbe9ad26c736af6823a1d4
StlMaris123 <stellamarisnjage@gmail.com>
2016-12-12 18:04:01 +0300

    provide host parameter in production and environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.default_url_options = { :host => 'localhost' }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'https://powerful-bayou-70411.herokuapp.com/' }
 
 
 end
