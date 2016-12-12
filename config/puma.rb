0b99e2cdbfd76ae2baa485764817976f31ae6238
StlMaris123 <stellamarisnjage@gmail.com>
2016-12-12 17:29:11 +0300

    remove configuration file for the production web server

--- config/puma.rb
+++ config/puma.rb
@@ -39,18 +39,18 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # or connections that may have been created at application boot, Ruby
 # cannot share connections between processes.
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = Integer(ENV['MAX_THREADS'] || 5)
+# threads threads_count, threads_count
 
-preload_app!
+# preload_app!
 
-rackup DefaultRackup
-port ENV['PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+# rackup DefaultRackup
+# port ENV['PORT'] || 3000
+# environment ENV['RACK_ENV'] || 'development'
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection
+# end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
