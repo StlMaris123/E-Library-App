2009a4ea80a6c7a3b5132648cb7edef782c475dd
StlMaris123 <stellamarisnjage@gmail.com>
2016-12-16 22:07:33 +0300

    add serve assets

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.serve_fixed_assets = true
+# config.serve_fixed_assets = true
 
 module ELibrary
   class Application < Rails::Application
