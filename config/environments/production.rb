e122210b8f3fb6c3eb4419f44d480a8f89771df1
StlMaris123 <stellamarisnjage@gmail.com>
2016-12-21 12:50:08 +0300

    remove ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,6 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  http.use_ssl = true if domain =~ /^https/
   config.action_mailer.delivery_method = :smtp
   #SMTP settings for gmail
   config.action_mailer.smtp_settings = {
